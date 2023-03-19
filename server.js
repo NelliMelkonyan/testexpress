2d8938b3b8cf45c8d06565a2c2d0a24e7d8fdb31
NelliMelkonyan <nelli.melkonyan.97@mail.ru>
2023-03-19 19:08:37 +0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  	console.log(req)
+  	console.log(req.headers['cf-connecting-ip'])
 	res.send('hello world')
 })
 
